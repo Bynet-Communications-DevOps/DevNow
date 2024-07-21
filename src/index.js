c4224bb10acb9059d41ec1c446308d8c0925a1a9
team-a <team-a@example.com>
2024-07-21 12:28:16 +0300

    commit meassage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-console.log('Welcome to Dev.Now color blidness test');
+console.log('I am a part of dev.now');
 
 function some_function(color) {
   console.log(color);
   const circle = document.getElementById('circleID');
   circle.style = `background-color:${color}`;
+}
+function joke() {
+  var url = https: //api.chucknorris.io/jokes/random;
+  var result;
+  
+  result = url.search(/cat/i);
+  var el = result.body;
+  var text = el.innertext;
+  
+  document.write(result);
+  
+
+  
 }
