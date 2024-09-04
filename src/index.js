f9c7b2a59f652df1b2e8da6f077d379409cc8a87
shirbarzilay <shir.barzilay@gmail.com>
2024-09-04 14:17:57 +0300

    solved q.5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-console.log('Welcome to Dev.Now color blidness test');
+console.log('I am a part of dev.now');
 
 function some_function(color) {
   console.log(color);
   const circle = document.getElementById('circleID');
   circle.style = `background-color:${color}`;
 }
+
 
 
